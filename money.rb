f866bf1818dada010d50aca565bf441295c32df5
Piotr Klosinski <mybettingstuff@gmail.com>
2015-05-04 15:28:39 +0200

    [#2] add .from_usd, .from_eur and .from_gbp methods; add #amount and #currency methods, minor refactoring

--- money.rb
+++ money.rb
@@ -4,17 +4,31 @@ class Money
     @currency = currency.to_s
   end
 
+  [:from_usd, :from_eur, :from_gbp].each do |method_name|
+    define_singleton_method method_name do |amount|
+      self.new(amount, method_name.to_s.split('_').last.upcase)
+    end
+  end
+
   def to_s
-    "#{amount_with_two_decimals} #{@currency}"
+    "#{amount_with_two_decimals} #{currency}"
   end
 
   def inspect
     "#<Money #{self.to_s}>"
   end
 
+  def amount
+    @amount
+  end
+
+  def currency
+    @currency
+  end
+
   private
 
   def amount_with_two_decimals
-    sprintf('%.2f', @amount)
+    sprintf('%.2f', amount)
   end
 end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,17 +1,51 @@
 require_relative '../money'
 
 describe Money do
-  let(:money) { Money.new(10, 'USD') }
+  describe 'Class methods' do
+    describe '.from_usd' do
+      let(:money_usd) { Money.from_usd(10) }
 
-  describe '#to_s' do
-    it 'should return a properly formatted string' do
-      expect(money.to_s).to eq('10.00 USD')
+      it 'should create an expected Money instance' do
+        expect(money_usd).to be_an_instance_of(Money)
+        expect(money_usd.currency).to eq('USD')
+        expect(money_usd.amount).to eq(10)
+      end
+    end
+
+    describe '.from_eur' do
+      let(:money_eur) { Money.from_eur(10) }
+
+      it 'should create an expected Money instance' do
+        expect(money_eur).to be_an_instance_of(Money)
+        expect(money_eur.currency).to eq('EUR')
+        expect(money_eur.amount).to eq(10)
+      end
+    end
+
+    describe '.from_gbp' do
+      let(:money_gbp) { Money.from_gbp(10) }
+
+      it 'should create an expected Money instance' do
+        expect(money_gbp).to be_an_instance_of(Money)
+        expect(money_gbp.currency).to eq('GBP')
+        expect(money_gbp.amount).to eq(10)
+      end
     end
   end
 
-  describe '#inspect' do
-    it 'should return a properly formatted string' do
-      expect(money.inspect).to eq('#<Money 10.00 USD>')
+  describe 'Instance methods' do
+    let(:money) { Money.new(10, 'USD') }
+
+    describe '#to_s' do
+      it 'should return a properly formatted string' do
+        expect(money.to_s).to eq('10.00 USD')
+      end
+    end
+
+    describe '#inspect' do
+      it 'should return a properly formatted string' do
+        expect(money.inspect).to eq('#<Money 10.00 USD>')
+      end
     end
   end
 end
